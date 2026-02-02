ada216ae010b97d7735d50895bd934000e07867d
WinerGero <korykin.27@mail.ru>
2026-02-02 20:47:40 +0200

    maked parse deteal of the only one article

--- PubMed_pasrer.py
+++ PubMed_pasrer.py
@@ -1,8 +1,13 @@
 import json
 import os
-from Bio import Entrez
+import logging
 from dotenv import load_dotenv
 
+from Bio import Entrez
+
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -47,23 +52,55 @@ class Parser():
         articles_list = records.get('PubmedArticle', [])
         return articles_list
 
+    def parse_article(self, article_data: dict):
+        # Извлечение необходимых данных из статьи
+        try:
+            article = article_data['MedlineCitation']['Article']
+            
+            # Название статьи
+            title = article.get('ArticleTitle', 'N/A')
+            
+            # Ссылка на статью в PubMed
+            abstract_list = article.get('Abstract', {}).get('AbstractText', [])
+            abstract = " ".join([str(part) for part in abstract_list]) if abstract_list else 'N/A'
+            
+            # Год публикации
+            pub_date = article['Journal']['JournalIssue']['PubDate'].get('Year', 'N/A')
+            
+            # Ссылка
+            pmid = article_data['MedlineCitation']['PMID']
+            url = f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/"
+            
+            if not abstract:
+                return None # Пропустить статьи без абстракта
+            
+            return {
+                "title": title,
+                "abstract": abstract,
+                "year": pub_date,
+                "url": url,
+                "pmid": str(pmid)
+            }
+        except KeyError as e:
+            logging.error(f"KeyError: {e} in article ID {article.get('MedlineCitation', {}).get('PMID', 'Unknown')}")
+            return None
+
 if __name__ == "__main__":
     query = "(Alzheimer's disease[Title/Abstract]) AND (therapeutic targets[Title/Abstract] OR drug targets[Title/Abstract])"
     
+    # Инициализация парсера и поиск статей
     parser = Parser(query=query, max_count=10)
     id_list = parser.search_pubmed_articles()
     
+    # Получение подробной информации о статьях
     if id_list:
         articles  = parser.fetch_details(id_list)
         print("Count of article IDs fetched:", len(id_list))
         print("Article IDs:", id_list)
         print("Fetched records:", len(articles ))
-        
-        for i, article in enumerate(articles[:2]):
-            title = article['MedlineCitation']['Article']['ArticleTitle']
-            print(f"{i+1}. {title}")
-            
-        print("Keys in the first article:", list(articles[0].keys()) if articles else "No articles to show keys for")
     else:
         print("No articles found.")
         
+    # Извлечение необходимых данных из статьи
+    parsed_article = parser.parse_article(articles[0])
+    print("Parsed article:", parsed_article)
